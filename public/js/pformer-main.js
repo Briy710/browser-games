1eb121d51c65fe004d95ce74edbc16a43ef23411
Brittany <heyjessie510@gmail.com>
2017-05-04 14:20:15 -0700

    still atempmting to render game

--- public/js/pformer-main.js
+++ public/js/pformer-main.js
@@ -1,13 +1,21 @@
+function Hero(game, x, y) {
+  Phaser.Sprite.call(this, game, x, y, 'hero');
+  this.anchor.set(0.5, 0.5);
+}
+Hero.prototype = Object.create(Phaser.Sprite.prototype);
+Hero.prototype.constructor = Hero;
+
 PlayState = {}
 PlayState.preload = function() {
-  this.game.load.image('background', 'img/background.png');
   this.game.load.json('level:1', 'data/level01');
   this.game.load.image('ground', 'img/ground.png');
-    this.game.load.image('grass:8x1', 'img/grass_8x1.png');
-    this.game.load.image('grass:6x1', 'img/grass_6x1.png');
-    this.game.load.image('grass:4x1', 'img/grass_4x1.png');
-    this.game.load.image('grass:2x1', 'img/grass_2x1.png');
-    this.game.load.image('grass:1x1', 'img/grass_1x1.png');
+  this.game.load.image('grass:8x1', 'img/grass_8x1.png');
+  this.game.load.image('grass:6x1', 'img/grass_6x1.png');
+  this.game.load.image('grass:4x1', 'img/grass_4x1.png');
+  this.game.load.image('grass:2x1', 'img/grass_2x1.png');
+  this.game.load.image('grass:1x1', 'img/grass_1x1.png');
+  this.game.load.image('background', 'img/background.png');
+  this.gme.load.image('hero', 'img/hero_stopped.png');
 };
 PlayState.create = function() {
   this.loadLevel(this.game.cache.getJSON('level:1'));
@@ -15,6 +23,11 @@ PlayState.create = function() {
 };
 PlayState._loadLevel = function (data) {
   data.platforms.forEach(this._spawnPlatform, this);
+  this._spawnCharacters({hero: data.hero});
+};
+PlayState._spawnCharacters = function(data) {
+  this.hero = new Hero(this.game, data.hero.x, data.hero.y)
+  this.game.add.existing(this.hero)
 };
 PlayState._spawnPlatform = function (platform) {
   this.game.add.sprite(platform.x, platform.y, platform.image);
