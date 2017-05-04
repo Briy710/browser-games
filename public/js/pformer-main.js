3fc4c5bf2aec2875802bcc7a97d5a556ce53da3c
Brittany <heyjessie510@gmail.com>
2017-05-04 11:04:12 -0700

    added platform images, still sill not render on localhost:8080

--- public/js/pformer-main.js
+++ public/js/pformer-main.js
@@ -1,16 +1,26 @@
 PlayState = {}
 PlayState.preload = function() {
-  this.game.load.image('background', 'img/background.png')
-  this.game.load.json('level:1', 'data/level01')
-}
+  this.game.load.image('background', 'img/background.png');
+  this.game.load.json('level:1', 'data/level01');
+  this.game.load.image('ground', 'img/ground.png');
+    this.game.load.image('grass:8x1', 'img/grass_8x1.png');
+    this.game.load.image('grass:6x1', 'img/grass_6x1.png');
+    this.game.load.image('grass:4x1', 'img/grass_4x1.png');
+    this.game.load.image('grass:2x1', 'img/grass_2x1.png');
+    this.game.load.image('grass:1x1', 'img/grass_1x1.png');
+};
 PlayState.create = function() {
-  tthis.loadLevel(this.game.cache.getJSON('level:1'))
-}
-PlayState._loadLevel = function (datd) {
-  console.log(data)
-}
+  this.loadLevel(this.game.cache.getJSON('level:1'));
+  this.game.add.image(0, 0, 'background');
+};
+PlayState._loadLevel = function (data) {
+  data.platforms.forEach(this._spawnPlatform, this);
+};
+PlayState._spawnPlatform = function (platform) {
+  this.game.add.sprite(platform.x, platform.y, platform.image);
+};
 window.onLoad = function() {
-  let game = newPhaser.Game(960, 600, Phaser.AUTO, 'game')
-  game.state.add('play', PlayState)
-  game.state.start('play')
-}
+  let game = new Phaser.Game(960, 600, Phaser.AUTO, 'game');
+  game.state.add('play', PlayState);
+  game.state.start('play');
+};
